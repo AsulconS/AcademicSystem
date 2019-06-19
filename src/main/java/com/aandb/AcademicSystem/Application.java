5c872776a20afc28abbb581ad39380f1d6df6cfb
AsulconS <rolandobedregal321@gmail.com>
2019-06-19 02:09:57 -0500

    Changed to Spring MVC

--- src/main/java/com/aandb/AcademicSystem/Application.java
+++ src/main/java/com/aandb/AcademicSystem/Application.java
@@ -1,18 +1,36 @@
 package com.aandb.AcademicSystem;
 
+import java.sql.SQLException;
+
 import javax.annotation.PreDestroy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.aandb.AcademicSystem.dao.StudentDAO;
+
 @SpringBootApplication
 public class Application
 {
     private static ConfigurableApplicationContext context;
+    public static StudentDAO studentDAO;
     
     public static void main(String[] args)
     {
+        String jdbcURL = "jdbc:mysql://localhost:3306/academicSystemDB";
+        String jdbcUsername = "root";
+        String jdbcPassword = "root";
+        
+        try
+        {
+            studentDAO = new StudentDAO(jdbcURL, jdbcUsername, jdbcPassword);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Failed to create StudentDAO!");
+        }
+        
         context = SpringApplication.run(Application.class, args);
     }
     
--- src/main/java/com/aandb/AcademicSystem/controller/MainController.java
+++ src/main/java/com/aandb/AcademicSystem/controller/MainController.java
@@ -1,9 +1,13 @@
 package com.aandb.AcademicSystem.controller;
 
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.aandb.AcademicSystem.Application;
 
 @Controller
 public class MainController
@@ -11,8 +15,14 @@ public class MainController
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model)
     {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+    
+    @RequestMapping("/students")
+    public String students(Model model) throws SQLException
+    {
+        model.addAttribute("students", Application.studentDAO.listStudents());
         return "tables";
-        //model.addAttribute("name", name);
-        //return "greeting";
     }
 }
