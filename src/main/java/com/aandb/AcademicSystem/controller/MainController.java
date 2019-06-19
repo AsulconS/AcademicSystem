bd571d417f0875645a43ee7749f623c9124e3836
AsulconS <rolandobedregal321@gmail.com>
2019-06-19 00:29:11 -0500

    Added SB Admin 2

--- src/main/java/com/aandb/AcademicSystem/controller/MainController.java
+++ src/main/java/com/aandb/AcademicSystem/controller/MainController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/AcademicSystem")
 public class MainController
 {
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model)
     {
-        model.addAttribute("name", name);
-        return "greeting";
+        return "tables";
+        //model.addAttribute("name", name);
+        //return "greeting";
     }
 }
